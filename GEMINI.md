d603b0daf03d315d7eab972decd450fb75eb4b0e
lily <gentop>
2025-12-20 04:13:18 +1100

    Refactor main.c

---
+++ GEMINI.md
@@ -0,0 +1,3 @@
+There should be a gitignored directory named tweeta or tweetapus, or similar. This contains the source code for the tweetapus social media platform, in which this program is a desktop application for.
+If there is no such directory and you need reference data, ask the user to download the directory, or download it from https://github.com/tweetapus/tweetapus.
+Each time you make a major change, update the test suite to cover this.
--- main.c
+++ main.c
@@ -11,7 +11,16 @@
 #include <curl/curl.h>
 #include <json-glib/json-glib.h>
 
+#define API_URL "https://tweeta.tiago.zip/api/public-tweets"
 
+// Represents a single tweet
+struct Tweet {
+  gchar *content;
+  gchar *author_name;
+  gchar *author_username;
+};
+
+// Memory buffer for curl
 struct MemoryStruct {
   char *memory;
   size_t size;
@@ -25,8 +34,7 @@ WriteMemoryCallback(void *contents, size_t size, size_t nmemb, void *userp)
 
   char *ptr = realloc(mem->memory, mem->size + realsize + 1);
   if(ptr == NULL) {
-    /* out of memory! */
-    printf("not enough memory (realloc returned NULL)\n");
+    g_critical("not enough memory (realloc returned NULL)");
     return 0;
   }
 
@@ -38,38 +46,57 @@ WriteMemoryCallback(void *contents, size_t size, size_t nmemb, void *userp)
   return realsize;
 }
 
-static void
-fetch_tweets(GtkWidget *list_box)
+static gboolean
+fetch_url(const gchar *url, struct MemoryStruct *chunk)
 {
-  CURL *curl_handle;
-  CURLcode res;
+    CURL *curl_handle;
+    CURLcode res;
 
-  struct MemoryStruct chunk;
+    chunk->memory = malloc(1);
+    chunk->size = 0;
 
-  chunk.memory = malloc(1);
-  chunk.size = 0;
+    curl_handle = curl_easy_init();
+    if (!curl_handle) {
+        g_critical("curl_easy_init() failed");
+        free(chunk->memory);
+        chunk->memory = NULL;
+        return FALSE;
+    }
 
-  curl_global_init(CURL_GLOBAL_ALL);
-  curl_handle = curl_easy_init();
-  curl_easy_setopt(curl_handle, CURLOPT_URL, "https://tweeta.tiago.zip/api/public-tweets");
-  curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
-  curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, (void *)&chunk);
-  curl_easy_setopt(curl_handle, CURLOPT_USERAGENT, "libcurl-agent/1.0");
-  res = curl_easy_perform(curl_handle);
+    curl_easy_setopt(curl_handle, CURLOPT_URL, url);
+    curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
+    curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, (void *)chunk);
+    curl_easy_setopt(curl_handle, CURLOPT_USERAGENT, "libcurl-agent/1.0");
 
-  if(res != CURLE_OK) {
-    fprintf(stderr, "curl_easy_perform() failed: %s\n",
-            curl_easy_strerror(res));
-  }
-  else {
-    JsonParser *parser = json_parser_new();
+    res = curl_easy_perform(curl_handle);
+
+    if (res != CURLE_OK) {
+        g_critical("curl_easy_perform() failed: %s", curl_easy_strerror(res));
+        free(chunk->memory);
+        chunk->memory = NULL;
+        chunk->size = 0;
+        curl_easy_cleanup(curl_handle);
+        return FALSE;
+    }
+
+    curl_easy_cleanup(curl_handle);
+    return TRUE;
+}
+
+static GList*
+parse_tweets(const gchar *json_data)
+{
+    JsonParser *parser;
     GError *error = NULL;
-    json_parser_load_from_data(parser, chunk.memory, -1, &error);
+    GList *tweets = NULL;
+
+    parser = json_parser_new();
+    json_parser_load_from_data(parser, json_data, -1, &error);
     if (error) {
         g_warning("Unable to parse json: %s", error->message);
         g_error_free(error);
         g_object_unref(parser);
-        return;
+        return NULL;
     }
 
     JsonNode *root_node = json_parser_get_root(parser);
@@ -77,37 +104,103 @@ fetch_tweets(GtkWidget *list_box)
     JsonArray *posts = json_object_get_array_member(root_object, "posts");
 
     for (guint i = 0; i < json_array_get_length(posts); i++) {
-      JsonNode *post_node = json_array_get_element(posts, i);
-      JsonObject *post_object = json_node_get_object(post_node);
-      const gchar *content = json_object_get_string_member(post_object, "content");
+        JsonNode *post_node = json_array_get_element(posts, i);
+        JsonObject *post_object = json_node_get_object(post_node);
+        JsonObject *author_object = json_object_get_object_member(post_object, "author");
 
-      JsonObject *author_object = json_object_get_object_member(post_object, "author");
-      const gchar *author_name = json_object_get_string_member(author_object, "name");
-      const gchar *author_username = json_object_get_string_member(author_object, "username");
-      gchar *author_str = g_strdup_printf("%s (@%s)", author_name, author_username);
+        struct Tweet *tweet = g_new(struct Tweet, 1);
+        tweet->content = g_strdup(json_object_get_string_member(post_object, "content"));
+        tweet->author_name = g_strdup(json_object_get_string_member(author_object, "name"));
+        tweet->author_username = g_strdup(json_object_get_string_member(author_object, "username"));
 
-
-      GtkWidget *box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
-      GtkWidget *author_label = gtk_label_new(author_str);
-      GtkWidget *content_label = gtk_label_new(content);
-
-      gtk_label_set_xalign(GTK_LABEL(author_label), 0.0);
-      gtk_label_set_xalign(GTK_LABEL(content_label), 0.0);
-      gtk_label_set_line_wrap(GTK_LABEL(content_label), TRUE);
-
-      gtk_box_pack_start(GTK_BOX(box), author_label, FALSE, FALSE, 0);
-      gtk_box_pack_start(GTK_BOX(box), content_label, FALSE, FALSE, 0);
-
-      gtk_list_box_insert(GTK_LIST_BOX(list_box), box, -1);
-      g_free(author_str);
+        tweets = g_list_append(tweets, tweet);
     }
 
     g_object_unref(parser);
-  }
+    return tweets;
+}
 
-  curl_easy_cleanup(curl_handle);
-  free(chunk.memory);
-  curl_global_cleanup();
+static void
+free_tweet(gpointer data)
+{
+    struct Tweet *tweet = data;
+    g_free(tweet->content);
+    g_free(tweet->author_name);
+    g_free(tweet->author_username);
+    g_free(tweet);
+}
+
+static void
+free_tweets(GList *tweets)
+{
+    g_list_free_full(tweets, free_tweet);
+}
+
+static GtkWidget*
+create_tweet_widget(struct Tweet *tweet)
+{
+    GtkWidget *box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
+    gchar *author_str = g_strdup_printf("%s (@%s)", tweet->author_name, tweet->author_username);
+
+    GtkWidget *author_label = gtk_label_new(author_str);
+    gtk_label_set_xalign(GTK_LABEL(author_label), 0.0);
+
+    GtkWidget *content_label = gtk_label_new(tweet->content);
+    gtk_label_set_xalign(GTK_LABEL(content_label), 0.0);
+    gtk_label_set_line_wrap(GTK_LABEL(content_label), TRUE);
+
+    gtk_box_pack_start(GTK_BOX(box), author_label, FALSE, FALSE, 0);
+    gtk_box_pack_start(GTK_BOX(box), content_label, FALSE, FALSE, 0);
+
+    g_free(author_str);
+
+    return box;
+}
+
+static void
+populate_tweet_list(GtkListBox *list_box, GList *tweets)
+{
+    // Clear existing items
+    GList *children, *iter;
+    children = gtk_container_get_children(GTK_CONTAINER(list_box));
+    for(iter = children; iter != NULL; iter = g_list_next(iter))
+        gtk_widget_destroy(GTK_WIDGET(iter->data));
+    g_list_free(children);
+
+    // Add new items
+    for (GList *l = tweets; l != NULL; l = l->next) {
+        GtkWidget *tweet_widget = create_tweet_widget(l->data);
+        gtk_list_box_insert(list_box, tweet_widget, -1);
+    }
+}
+
+static void
+load_tweets(GtkListBox *list_box)
+{
+    struct MemoryStruct chunk;
+
+    if (fetch_url(API_URL, &chunk)) {
+        GList *tweets = parse_tweets(chunk.memory);
+        if (tweets) {
+            populate_tweet_list(list_box, tweets);
+            free_tweets(tweets);
+        }
+        free(chunk.memory);
+    } else {
+        // Optionally, show an error message in the UI
+        GtkWidget *error_label = gtk_label_new("Failed to load tweets.");
+        gtk_list_box_insert(list_box, error_label, -1);
+    }
+}
+
+static GtkWidget*
+create_window()
+{
+    GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(GTK_WINDOW(window), "Tweetapus");
+    gtk_window_set_default_size(GTK_WINDOW(window), 1024, 768);
+    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+    return window;
 }
 
 int main(int argc, char *argv[]) {
@@ -116,11 +209,9 @@ int main(int argc, char *argv[]) {
     GtkWidget *scrolled_window;
 
     gtk_init(&argc, &argv);
+    curl_global_init(CURL_GLOBAL_ALL);
 
-    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_title(GTK_WINDOW(window), "Tweetapus");
-    gtk_window_set_default_size(GTK_WINDOW(window), 1024, 768);
-    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+    window = create_window();
 
     scrolled_window = gtk_scrolled_window_new(NULL, NULL);
     gtk_container_add(GTK_CONTAINER(window), scrolled_window);
@@ -128,11 +219,13 @@ int main(int argc, char *argv[]) {
     list_box = gtk_list_box_new();
     gtk_container_add(GTK_CONTAINER(scrolled_window), list_box);
 
-    fetch_tweets(list_box);
+    load_tweets(GTK_LIST_BOX(list_box));
 
     gtk_widget_show_all(window);
 
     gtk_main();
+
+    curl_global_cleanup();
 
     return 0;
 }
